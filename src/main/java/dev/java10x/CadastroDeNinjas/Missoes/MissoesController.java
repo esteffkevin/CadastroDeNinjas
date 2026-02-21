e56e77759f64bfa81416d53c4b96cf83ed523ec2
Kevin Esteff <kevinesteff@Kevins-MacBook-Pro.local>
2026-02-21 12:35:23 +0000

    feat: Cria as rotas do NinjaController com mock

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,33 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController // <- isso aqui vai ser uma rota para as nossas APIs
+@RequestMapping("missoes") // <- vai mapear as APIs
+public class MissoesController {
+
+    // GET -- Mandar uma requisicao para mostrar as missoes
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso";
+    }
+
+    // POST -- Mandar uma requisicao para criar as missoes
+    @PostMapping("/criar") // <- "O usuário vai mandar pra mim"
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    // PUT -- Mandar uma requisicao para alterar as missoes
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    // DELETE -- Mandar uma requisicao para deletar as missoes
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missao deletada com sucesso";
+    }
+
+}
