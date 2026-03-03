f76168afaa2ed773cad81d65e50a534e0e2cd201
Kevin Esteff <kevinesteff@Kevins-MacBook-Pro.local>
2026-03-03 17:50:34 +0000

    fix: Inclui a annotation @JsonIgnore no MissoesModel para previnir o erro de serializacao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,4 +1,5 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -26,6 +27,7 @@ public class MissoesModel {
     // @OneToMany - Uma missao pode ter vários ninjas
     // mappedBy - nome da cluna da outra classe que vamos trabalhar
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
 }
